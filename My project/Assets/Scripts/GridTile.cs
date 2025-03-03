77a24106100981a83cbeafc1f5d2ff0c2d91abf5
rose-delaney <rose.delaney@mymail.champlain.edu>
2025-03-02 21:08:51 -0500

    Did a little bit of color stuff

--- My project/Assets/Scripts/GridTile.cs
+++ My project/Assets/Scripts/GridTile.cs
@@ -3,4 +3,17 @@ using UnityEngine;
 public class GridTile : MonoBehaviour
 {
     public bool solid;
+    private SpriteRenderer _spriteRenderer;
+    private Color _defaultColor;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultColor = _spriteRenderer.color;
+    }
+
+    public void SetColor(Color color)
+    {
+        _spriteRenderer.color = color;
+    }
 }
